70e2e758775e19d31c5144ad4e864198ff63c040
Deven Patel <dpatel@shareablesapp.com>
2024-10-20 17:48:37 -0400

    eas changes

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "Shareables",
     slug: "shareablesapp",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "light",
